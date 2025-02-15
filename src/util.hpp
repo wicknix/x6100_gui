479ff12df6c47d0601fed57eeecb37671da28a19
Georgy Dyuldin <g.dyuldin@gmail.com>
2025-02-15 09:18:12 +0300

    Include missed file

---
+++ src/util.hpp
@@ -0,0 +1,51 @@
+/*
+ *  SPDX-License-Identifier: LGPL-2.1-or-later
+ *
+ *  Xiegu X6100 LVGL GUI
+ *
+ *  Copyright (c) 2022-2023 Belousov Oleg aka R1CBU
+ */
+
+#pragma once
+
+#include <condition_variable>
+#include <mutex>
+#include <queue>
+
+
+
+template <typename T> inline T clip(T x, T min, T max) {
+    if (x < min) {
+        return min;
+    } else if (x > max) {
+        return max;
+    }
+
+    return x;
+}
+
+template <typename T> class TSQueue {
+  private:
+    std::queue<T>           m_queue;
+    std::mutex              m_mutex;
+    std::condition_variable m_cond;
+
+  public:
+    void push(T item) {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_queue.push(item);
+        m_cond.notify_one();
+    }
+
+    T pop() {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_cond.wait(lock, [this]() { return !m_queue.empty(); });
+        T item = m_queue.front();
+        m_queue.pop();
+        return item;
+    }
+    bool empty() {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        return m_queue.empty();
+    }
+};
