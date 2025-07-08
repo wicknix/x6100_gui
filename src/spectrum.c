cfd71304e86f1c55ecab9b67e10bbbc36a7c4893
wicknix <39230578+wicknix@users.noreply.github.com>
2025-07-07 21:02:15 -0500

    Adjust spectrum color to resemble the G90’s green spectrum
    and make the filter color easier to see and match the G90’s

--- src/spectrum.c
+++ src/spectrum.c
@@ -97,7 +97,7 @@ static void spectrum_draw_cb(lv_event_t *e) {
 
     lv_draw_line_dsc_init(&main_line_dsc);
 
-    main_line_dsc.color = lv_color_hex(0xAAAAAA);
+    main_line_dsc.color = lv_color_hex(0x00B300);
     main_line_dsc.width = 1;
 
     lv_draw_line_dsc_init(&peak_line_dsc);
@@ -165,7 +165,7 @@ static void spectrum_draw_cb(lv_event_t *e) {
 
     lv_draw_rect_dsc_init(&rect_dsc);
 
-    rect_dsc.bg_color = bg_color;
+    rect_dsc.bg_color = lv_color_hex(0xFFEA00);
     rect_dsc.bg_opa   = LV_OPA_50;
 
     int32_t w_hz = width_hz / zoom_factor;
